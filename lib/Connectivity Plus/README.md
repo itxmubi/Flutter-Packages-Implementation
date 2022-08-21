937b3051a1ba1af5d76e1895ff590fe667a6cceb
Mubashir Nawaz <63649866+itxmubi@users.noreply.github.com>
2022-08-21 15:05:52 +0500

    Connectivity plus pakage added

---
+++ lib/Connectivity Plus/README.md
@@ -0,0 +1,16 @@
+# Connectivity Plus 
+
+
+
+
+
+## Getting Started
+
+This package detects the connectivity of the app, whether the Device is connected with internet,
+wifi Bluetooth, or Mobile data.
+
+
+
+- [Connectivity Plus](https://pub.dev/packages/connectivity_plus)
+<!-- - [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook) -->
+
---
+++ lib/Connectivity Plus/connectivity_plus_main.dart
@@ -0,0 +1,90 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'dart:async';
+import 'dart:developer' as developer;
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+
+class ConnectivityPlusClass extends StatefulWidget {
+  const ConnectivityPlusClass({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ConnectivityPlusClassState createState() => _ConnectivityPlusClassState();
+}
+
+class _ConnectivityPlusClassState extends State<ConnectivityPlusClass> {
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connectivity Plus '),
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+           _connectionStatus == ConnectivityResult.wifi ? 
+           const   Icon(Icons.wifi,size: 40,) : _connectionStatus == ConnectivityResult.mobile ? 
+            const  Icon(Icons.perm_data_setting,size: 40,)
+           : const  Icon(Icons.not_interested,size: 40,),
+       const    SizedBox(height: 20,),
+
+              Text('Connection Status: ${_connectionStatus.toString()}'),
+              ],
+          )),
+    );
+  }
+}
