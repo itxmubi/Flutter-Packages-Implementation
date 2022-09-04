a4ecf81e11d3a22fff33a4471bf4709f66afd288
Mubashir Nawaz <63649866+itxmubi@users.noreply.github.com>
2022-09-04 23:41:38 +0500

    Image Picker Package added

--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_packages_implementation/Carousel%20Slider/carousel_slider_example.dart';
+import 'package:flutter_packages_implementation/Google%20Fonts/google_fonts_example.dart';
+import 'package:flutter_packages_implementation/Image%20Picker/image_picker_example.dart';
 
 import '../Connectivity Plus/connectivity_plus_example.dart';
 import '../Device Info Plus/device_info_example.dart';
@@ -16,48 +19,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List pagesNamese = [
+    "Share Plus Example",
+    "Image Picker Example",
+    "Flutter Svg Example",
+    "Flutter Toast Example",
+    "Lottie Example",
+    "Device Info Plus Example",
+    "Connectivity Plus Example",
+    "Facebook Audience Network",
+    "Carousel Slider",
+    "Google Fonts"
+  ];
+
+  List<Widget> pages = [
+    const SharePlusExample(),
+    const ImagePickerExample(),
+    const FlutterSvgExample(),
+    FlutterToastExample(),
+    const LottieExample(),
+    const DeviceInfoExample(),
+    const ConnectivityPlusExample(
+      title: "Connectivity Plus Example",
+    ),
+    const FacebookAdsPage(),
+    const CarouselSliderExample(),
+    const GoogleFontsExample(title: "Google Fonts Example")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Packages Implementation"),
+      appBar: AppBar(
+        title: const Text("Flutter Packages Implementation"),
       ),
-
       body: Column(children: [
-        ListTile(title: Text("Share Plus Example",),
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> SharePlusExample()));
-          
-        },),
-        ListTile(title: Text("Flutter Svg Example"),
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> FlutterSvgExample()));
-
-        },),
-        ListTile(title: Text("Flutter Toast Example"),
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> FlutterToastExample()));
-
-        },),
-        ListTile(title: Text("Lottie Example"),
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> LottieExample()));
-
-        },),
-        ListTile(title: Text("Device Info Plus Example"),
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> DeviceInfoExample()));
-
-        },),
-        ListTile(title: Text("Connectivity Plus Example"),
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> ConnectivityPlusExample(title: "Connectivity Plus Example",)));
-
-        },),
-        ListTile(title: Text("Facebook Audience Network Example"),
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> FacebookAdsPage()));
-
-        },),
+        for (int i = 0; i < pages.length; i++)
+          Column(
+            children: [
+              ListTile(
+                title: Text(pagesNamese[i]),
+                onTap: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => pages[i]));
+                },
+                trailing: const Icon(Icons.arrow_forward_ios),
+              ),
+            const  Divider()
+            ],
+          ),
       ]),
     );
   }
--- lib/Image Picker/image_picker_example.dart
+++ lib/Image Picker/image_picker_example.dart
@@ -12,32 +12,16 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ImagePickerExample extends StatefulWidget {
+  const ImagePickerExample({Key? key,}) : super(key: key);
+
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Image Picker Demo',
-      home: MyHomePage(title: 'Image Picker Example'),
-    );
-  }
+  State<ImagePickerExample> createState() => _ImagePickerExampleState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _ImagePickerExampleState extends State<ImagePickerExample> {
   List<XFile>? _imageFileList;
 
   void _setImageFileListFromFile(XFile? value) {
@@ -244,7 +228,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title!),
+        title: Text("Image Picker Example"),
       ),
       body: Center(
         child: !kIsWeb && defaultTargetPlatform == TargetPlatform.android
