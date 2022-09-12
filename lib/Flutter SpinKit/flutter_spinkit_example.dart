12e64dc214f7faf4140b142d0585224fd0e5bf77
Mubashir Nawaz <63649866+itxmubi@users.noreply.github.com>
2022-09-12 23:12:27 +0500

    Flutter SpinKit package added

---
+++ lib/Flutter SpinKit/flutter_spinkit_example.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_packages_implementation/Flutter%20SpinKit/showcase.dart';
+import 'package:flutter_packages_implementation/Flutter%20SpinKit/workspace.dart';
+
+class FlutterSpinkitExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: LayoutBuilder(
+              builder: (context, _) {
+                return IconButton(
+                  icon: const Icon(Icons.play_circle_filled),
+                  iconSize: 50.0,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute<void>(
+                        builder: (BuildContext context) => const ShowCase(),
+                        fullscreenDialog: true,
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          const Positioned.fill(
+            child: Center(
+              child: WorkSpace(),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_packages_implementation/Shimmer/shimmer_example.dart';
 
-import '../Google Mobile Ads/native_ads_example.dart';
+import '../Carousel Slider/carousel_slider_example.dart';
+import '../Connectivity Plus/connectivity_plus_example.dart';
+import '../Device Info Plus/device_info_example.dart';
+import '../Facebook Audience Network/facebook_ads_example.dart';
+import '../Flutter SpinKit/flutter_spinkit_example.dart';
+import '../Flutter Svg/flutter_svg_example.dart';
+import '../Flutter Toast/flutter_toast_example.dart';
+import '../Google Fonts/google_fonts_example.dart';
+import '../Image Picker/image_picker_example.dart';
+import '../Lottie/lottie_example.dart';
+import '../Share Plus/share_plus_example.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,35 +22,37 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List pagesNamese = [
-    "Native Ad Example",
-    "Shimmer Example",
-    // "Share Plus Example",
-    // "Image Picker Example",
-    // "Flutter Svg Example",
-    // "Flutter Toast Example",
-    // "Lottie Example",
-    // "Device Info Plus Example",
-    // "Connectivity Plus Example",
-    // "Facebook Audience Network",
-    // "Carousel Slider",
-    // "Google Fonts"
+    "Flutter SpinKit Example"
+        // "Native Ad Example",/
+        "Shimmer Example",
+    "Share Plus Example",
+    "Image Picker Example",
+    "Flutter Svg Example",
+    "Flutter Toast Example",
+    "Lottie Example",
+    "Device Info Plus Example",
+    "Connectivity Plus Example",
+    "Facebook Audience Network",
+    "Carousel Slider",
+    "Google Fonts"
   ];
 
   List<Widget> pages = [
-    const NativeAdExample(),
+    FlutterSpinkitExample(),
+    // const NativeAdExample(),
     ShimmerExample(),
-    // const SharePlusExample(),
-    // const ImagePickerExample(),
-    // const FlutterSvgExample(),
-    // FlutterToastExample(),
-    // const LottieExample(),
-    // const DeviceInfoExample(),
-    // const ConnectivityPlusExample(
-    //   title: "Connectivity Plus Example",
-    // ),
-    // const FacebookAdsPage(),
-    // const CarouselSliderExample(),
-    // const GoogleFontsExample(title: "Google Fonts Example")
+    const SharePlusExample(),
+    const ImagePickerExample(),
+    const FlutterSvgExample(),
+    FlutterToastExample(),
+    const LottieExample(),
+    const DeviceInfoExample(),
+    const ConnectivityPlusExample(
+      title: "Connectivity Plus Example",
+    ),
+    const FacebookAdsPage(),
+    const CarouselSliderExample(),
+    const GoogleFontsExample(title: "Google Fonts Example")
   ];
 
   @override
