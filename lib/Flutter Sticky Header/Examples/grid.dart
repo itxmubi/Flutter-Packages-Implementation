e0b730998fe78c3f26633114d1a7fabef07bfd19
Mubashir Nawaz <mubashirnawaz751@gmail.com>
2023-04-24 15:10:05 +0400

    working on sticky header

---
+++ lib/Flutter Sticky Header/Examples/grid.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+
+import '../common.dart';
+
+class GridExample extends StatelessWidget {
+  const GridExample({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold(
+      title: 'Grid Example',
+      slivers: [
+        const _StickyHeaderGrid(index: 0),
+        const _StickyHeaderGrid(index: 1),
+        const _StickyHeaderGrid(index: 2),
+        const _StickyHeaderGrid(index: 3),
+      ],
+    );
+  }
+}
+
+class _StickyHeaderGrid extends StatelessWidget {
+  const _StickyHeaderGrid({
+    Key? key,
+    this.index,
+  }) : super(key: key);
+
+  final int? index;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverStickyHeader(
+      header: Header(index: index),
+      sliver: SliverGrid(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+        delegate: SliverChildBuilderDelegate(
+          (context, i) => GridTile(
+            footer: Container(
+              color: Colors.white.withOpacity(0.5),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Grid tile #$i',
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+            child: Card(
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ),
+          childCount: 9,
+        ),
+      ),
+    );
+  }
+}
