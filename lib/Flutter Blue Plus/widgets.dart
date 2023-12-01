fcffc6254da4c04e5f7885abf25738ccc6fa21c3
Mubashir Nawaz <mubashirnawaz751@gmail.com>
2023-12-01 17:32:09 +0400

    packages updated

--- lib/Flutter Blue Plus/widgets.dart
+++ lib/Flutter Blue Plus/widgets.dart
@@ -6,7 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class ScanResultTile extends StatelessWidget {
-  const ScanResultTile({Key? key, required this.result, this.onTap}) : super(key: key);
+  const ScanResultTile({Key? key, required this.result, this.onTap})
+      : super(key: key);
 
   final ScanResult result;
   final VoidCallback? onTap;
@@ -45,7 +46,10 @@ class ScanResultTile extends StatelessWidget {
           Expanded(
             child: Text(
               value,
-              style: Theme.of(context).textTheme.bodySmall?.apply(color: Colors.black),
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall
+                  ?.apply(color: Colors.black),
               softWrap: true,
             ),
           ),
@@ -55,7 +59,8 @@ class ScanResultTile extends StatelessWidget {
   }
 
   String getNiceHexArray(List<int> bytes) {
-    return '[${bytes.map((i) => i.toRadixString(16).padLeft(2, '0')).join(', ')}]'.toUpperCase();
+    return '[${bytes.map((i) => i.toRadixString(16).padLeft(2, '0')).join(', ')}]'
+        .toUpperCase();
   }
 
   String getNiceManufacturerData(Map<int, List<int>> data) {
@@ -64,7 +69,8 @@ class ScanResultTile extends StatelessWidget {
     }
     List<String> res = [];
     data.forEach((id, bytes) {
-      res.add('${id.toRadixString(16).toUpperCase()}: ${getNiceHexArray(bytes)}');
+      res.add(
+          '${id.toRadixString(16).toUpperCase()}: ${getNiceHexArray(bytes)}');
     });
     return res.join(', ');
   }
@@ -86,24 +92,27 @@ class ScanResultTile extends StatelessWidget {
       title: _buildTitle(context),
       leading: Text(result.rssi.toString()),
       trailing: ElevatedButton(
-        child: const Text('CONNECT'),
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.black,
           foregroundColor: Colors.white,
         ),
         onPressed: (result.advertisementData.connectable) ? onTap : null,
+        child: const Text('CONNECT'),
       ),
       children: <Widget>[
-        _buildAdvRow(context, 'Complete Local Name', result.advertisementData.localName),
-        _buildAdvRow(context, 'Tx Power Level', '${result.advertisementData.txPowerLevel ?? 'N/A'}'),
-        _buildAdvRow(context, 'Manufacturer Data', getNiceManufacturerData(result.advertisementData.manufacturerData)),
+        _buildAdvRow(
+            context, 'Complete Local Name', result.advertisementData.advName),
+        _buildAdvRow(context, 'Tx Power Level',
+            '${result.advertisementData.txPowerLevel ?? 'N/A'}'),
+        _buildAdvRow(context, 'Manufacturer Data',
+            getNiceManufacturerData(result.advertisementData.manufacturerData)),
         _buildAdvRow(
             context,
             'Service UUIDs',
             (result.advertisementData.serviceUuids.isNotEmpty)
                 ? result.advertisementData.serviceUuids.join(', ').toUpperCase()
                 : 'N/A'),
-        _buildAdvRow(context, 'Service Data', getNiceServiceData(result.advertisementData.serviceData)),
+        // _buildAdvRow(context, 'Service Data', getNiceServiceData(result.advertisementData.serviceData)),
       ],
     );
   }
@@ -113,7 +122,9 @@ class ServiceTile extends StatelessWidget {
   final BluetoothService service;
   final List<CharacteristicTile> characteristicTiles;
 
-  const ServiceTile({Key? key, required this.service, required this.characteristicTiles}) : super(key: key);
+  const ServiceTile(
+      {Key? key, required this.service, required this.characteristicTiles})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -125,10 +136,8 @@ class ServiceTile extends StatelessWidget {
           children: <Widget>[
             const Text('Service'),
             Text('0x${service.serviceUuid.toString().toUpperCase()}',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge
-                    ?.copyWith(color: Theme.of(context).textTheme.bodySmall?.color))
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    color: Theme.of(context).textTheme.bodySmall?.color))
           ],
         ),
         children: characteristicTiles,
@@ -179,30 +188,34 @@ class _CharacteristicTileState extends State<CharacteristicTile> {
                 const Text('Characteristic'),
                 Text(
                   '0x${widget.characteristic.characteristicUuid.toString().toUpperCase()}',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyLarge
-                      ?.copyWith(color: Theme.of(context).textTheme.bodySmall?.color),
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                      color: Theme.of(context).textTheme.bodySmall?.color),
                 ),
                 Row(
                   children: [
                     if (widget.characteristic.properties.read)
                       TextButton(
-                          child: Text("Read"),
+                          child: const Text("Read"),
                           onPressed: () async {
                             await widget.onReadPressed!();
                             setState(() {});
                           }),
                     if (widget.characteristic.properties.write)
                       TextButton(
-                          child: Text(widget.characteristic.properties.writeWithoutResponse ? "WriteNoResp" : "Write"),
+                          child: Text(widget.characteristic.properties
+                                  .writeWithoutResponse
+                              ? "WriteNoResp"
+                              : "Write"),
                           onPressed: () async {
                             await widget.onWritePressed!();
                             setState(() {});
                           }),
-                    if (widget.characteristic.properties.notify || widget.characteristic.properties.indicate)
+                    if (widget.characteristic.properties.notify ||
+                        widget.characteristic.properties.indicate)
                       TextButton(
-                          child: Text(widget.characteristic.isNotifying ? "Unsubscribe" : "Subscribe"),
+                          child: Text(widget.characteristic.isNotifying
+                              ? "Unsubscribe"
+                              : "Subscribe"),
                           onPressed: () async {
                             await widget.onNotificationPressed!();
                             setState(() {});
@@ -226,7 +239,12 @@ class DescriptorTile extends StatelessWidget {
   final VoidCallback? onReadPressed;
   final VoidCallback? onWritePressed;
 
-  const DescriptorTile({Key? key, required this.descriptor, this.onReadPressed, this.onWritePressed}) : super(key: key);
+  const DescriptorTile(
+      {Key? key,
+      required this.descriptor,
+      this.onReadPressed,
+      this.onWritePressed})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -237,8 +255,8 @@ class DescriptorTile extends StatelessWidget {
         children: <Widget>[
           const Text('Descriptor'),
           Text('0x${descriptor.descriptorUuid.toString().toUpperCase()}',
-              style:
-                  Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).textTheme.bodySmall?.color))
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  color: Theme.of(context).textTheme.bodySmall?.color))
         ],
       ),
       subtitle: StreamBuilder<List<int>>(
@@ -270,7 +288,8 @@ class DescriptorTile extends StatelessWidget {
 }
 
 class AdapterStateTile extends StatelessWidget {
-  const AdapterStateTile({Key? key, required this.adapterState}) : super(key: key);
+  const AdapterStateTile({Key? key, required this.adapterState})
+      : super(key: key);
 
   final BluetoothAdapterState adapterState;
 
@@ -299,4 +318,3 @@ SnackBar snackBarGood(String message) {
 SnackBar snackBarFail(String message) {
   return SnackBar(content: Text(message), backgroundColor: Colors.red);
 }
-
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,12 +10,14 @@ import connectivity_plus
 import device_info_plus
 import file_selector_macos
 import flutter_blue_plus
+import flutter_image_compress_macos
 import just_audio
 import location
 import mobile_scanner
 import path_provider_foundation
 import share_plus
 import url_launcher_macos
+import video_player_avfoundation
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -24,11 +26,13 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
